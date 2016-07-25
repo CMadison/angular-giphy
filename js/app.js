3039115a5b7fdfc2a3ccebf41ae638e6bcc6e354
Chris Madison <CKMadison4@gmail.com>
2016-07-25 08:00:20 -0700

    Rated R Giphy search complete with all base requirements. Next steps are to add pagination with first and last pages or infinite scroll.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,31 @@
-angular.module('GiphyApp', [])
+var app = angular.module('GiphyApp', [])
 
-.run(function() {
-  console.log('App has loaded!');
-});
+app.controller('GiphyCtrl', ['$scope', '$http', function($scope, $http) {
+  console.log('App loaded successfully');
+
+  // $scope.query = '';
+
+// Api call for search
+  $scope.getGifs = function() {
+    $http.get('http://api.giphy.com/v1/gifs/search', {
+// Api parameters
+      params: {
+        q: $scope.query,
+        api_key: 'dc6zaTOxFJmzC',
+        rating: 'r'
+      }
+    })
+    .then(function success(res) {
+      $scope.gifs = res.data.data;
+      console.log($scope.query, "success");
+      console.log('Odesza');
+    }, function error(res) {
+      console.log($scope.query, "Error");
+    });
+  }
+  console.log('Gifs are cooler than emojis.');
+}]);
+
+// app.run(function() {
+//   console.log('App has loaded!');
+// });
